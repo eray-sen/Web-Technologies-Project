1a91de9dd2eb314d62ff4c4a9645dfb3181c1f55
erraysen <eray.sen2@ogr.sakarya.edu.tr>
2022-04-24 19:13:32 +0300

    commit

--- Web projesi/Php_dosyaları/Kontrol.php
+++ Web projesi/Php_dosyaları/Kontrol.php
@@ -1,1 +1,33 @@
-echo "merhaba";
+<?php 
+require "Yonlendirme.php";
+session_start();
+$email = $_POST['email'];
+$password = $_POST['password'];
+$Data_email='g211210019@sakarya.edu.tr';
+$Data_password='g211210019';
+ 
+if($email and $password){
+{
+    if($email==$Data_email && $password==$Data_password)
+    {
+        echo("<h2> Hoş Geldiniz  $email " );
+
+    }
+    else{
+        echo("<h2> Bu bilgilere sahip bir kullanıcı bulunamadı. Bilgilerinizi kontrol ediniz.");
+      
+    }
+    
+}
+ 
+if($db_email == $email and $db_password == $password){
+echo "Giriş Başarılı Şekilde Yapıldı.";
+include("session.php");
+} else { echo "Kullanıcı adı veya parola yanlış.";
+header("location:login.html");
+}
+ 
+} else {
+header("location:login.php");
+}
+?>
